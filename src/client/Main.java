0e71a68512612eec0bbda1c3bd0d40fb991da9ae
derFlo-H <codingwithflorian@gmail.com>
2021-10-21 14:47:42 +0200

    added server and client

--- src/client/Main.java
+++ src/client/Main.java
@@ -1,10 +1,52 @@
 package client;
+import java.net.*;
+import java.io.*;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		try {
+			
+			Socket s = new Socket("localhost", 6666);
+			DataOutputStream dOut = new DataOutputStream(s.getOutputStream());
+			DataInputStream dIn = new DataInputStream(s.getInputStream());
+			
+			dIn.close();
+			dOut.close();
+			s.close();
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+ 
+	}
+	
+	public static String read(DataInputStream dIn) {
+		String text = "";
+		
+		try {
+			
+			text = (String) dIn.readUTF();
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		return text;
+	}
+	
+	public static void write(DataOutputStream dOut ,String text) {
+		
+		try {
+			
+			dOut.writeUTF(text);
+			dOut.flush();
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+				
 	}
 
 }
--- src/server/Main.java
+++ src/server/Main.java
@@ -1,10 +1,56 @@
 package server;
 
+import java.net.*;
+import java.io.*;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		try {
+			
+			ServerSocket ss = new ServerSocket(6666);
+			Socket s = ss.accept();
+			
+			DataInputStream dIn = new DataInputStream(s.getInputStream());
+			DataOutputStream dOut = new DataOutputStream(s.getOutputStream());
+			
+			dOut.close();
+			dIn.close();
+			s.close();
+			ss.close();
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public static String read(DataInputStream dIn) {
+		String text = "";
+		
+		try {
+			
+			text = (String) dIn.readUTF();
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		return text;
+	}
+	
+	public static void write(DataOutputStream dOut ,String text) {
+		
+		try {
+			
+			dOut.writeUTF(text);
+			dOut.flush();
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+				
 	}
 
 }
